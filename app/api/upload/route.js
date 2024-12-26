3f0c8423d22137782aca3f728fda6e88d0d5f616
erentomioke <ty3758621@gmail.com>
2024-12-26 11:12:09 +0330

    comit

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,59 +1,3 @@
-// import fs from 'fs';
-// import { writeFile } from 'fs/promises';
-// import { NextResponse } from 'next/server';
-// import path from 'path';
-// import formidable from 'formidable';
-// import { join } from 'path';
-// import { auth } from '@auth';
-
-
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
-
-
-// export async function POST(req) {
-//   try {
-//     const session =  auth()
-//     if (!session) {
-//       return NextResponse.json({ status: 400, error: 'invalidate request' });
-//     }
-//     const data = await req.formData();
-//     const file = data.get('file');
-//     console.log(file);
-//     if (!file) {
-//       return NextResponse.json({ status: 400, error: 'File not exist' });
-//     }
-//     const bytes = await file.arrayBuffer();
-//     const buffer = Buffer.from(bytes);
-//     const uploadDir = join(process.cwd(), 'public', 'uploads','post-content'); // Corrected to save in a public directory
-
-//     // Ensure the upload directory exists
-//     if (!fs.existsSync(uploadDir)) {
-//       fs.mkdirSync(uploadDir, { recursive: true });
-//     }
-//     const uniqueSuffix = Date.now() + '_' + Math.round(Math.random() * 10);
-//     const newFileName = `${uniqueSuffix}-${file.name}`
-//     const newFilePath = `${newFileName}`
-
-//     const filePath = join(uploadDir, file.name);
-//     await writeFile(filePath, buffer);
-//     await sharp(buffer).webp().toFile(webpFilePath);
-//     // console.log(`Open ${filePath} to see the uploaded file`);
-
-//     return NextResponse.json({ status: 200, message: 'Image upload successful', imageUrl: `/uploads/post-content/${file.name}` });
-//   } catch (error) {
-//     console.error('Upload error', error);
-//     return NextResponse.json(
-//       { error: 'Internal Server Error', details: error.message },
-//       { status: 500 }
-//     );
-//   }
-// }
-
-
 import fs from 'fs';
 import { writeFile } from 'fs/promises';
 import { NextResponse } from 'next/server';
@@ -62,12 +6,12 @@ import formidable from 'formidable';
 import sharp from 'sharp';
 import { auth } from '@auth';
 
-export const runtime = 'nodejs';
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+// export const runtime = 'nodejs';
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
 
 export async function POST(req) {
   try {
