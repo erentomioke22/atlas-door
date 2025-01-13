32a318388a4ae815baa3fcff35a06bc857987243
madmad <ereny3gers10@gmail.com>
2025-01-13 17:30:35 +0330

    jlno oihoih oih oih

--- components/BlockEditor/BlockEditor.jsx
+++ components/BlockEditor/BlockEditor.jsx
@@ -40,25 +40,26 @@ export const BlockEditor =
   ) => {
     // const leftSidebar = useSidebar()
     const menuContainerRef = useRef(null);
-    const {
-      editor,
-      // users,
-      //  collabState
-    } = useBlockEditor({
-      // aiToken,
-      ydoc,
-      // provider,
-      content,
-      onChange,
-      files,
-      setFiles,
-      setValue,
-      deletedFiles,
-      setDeletedFiles,
-      setEditorContent,
-      setDeletedPostFiles,
-      deletedPostFiles,contentImages, setContentImage,thumnailIndex,setThumnailIndex
-    });
+    // const {
+    //   editor,
+    //   // users,
+    //   //  collabState
+    // } = useBlockEditor({
+    //   // aiToken,
+    //   ydoc,
+    //   // provider,
+    //   content,
+    //   onChange,
+    //   files,
+    //   setFiles,
+    //   setValue,
+    //   deletedFiles,
+    //   setDeletedFiles,
+    //   setEditorContent,
+    //   setDeletedPostFiles,
+    //   deletedPostFiles,contentImages, setContentImage,thumnailIndex,setThumnailIndex
+    // });
+
     if (
       !editor
       // || !users
@@ -82,11 +83,11 @@ export const BlockEditor =
         <TableRowMenu editor={editor} appendTo={menuContainerRef} />
         <TableColumnMenu editor={editor} appendTo={menuContainerRef} /> */}
         <div className="space-y-5">
-          <EditorContent
+          {/* <EditorContent
             editor={editor}
             className="max-h-[500px] overflow-auto"
             ref={ref}
-          />
+          /> */}
           <LinkMenu editor={editor} appendTo={menuContainerRef} />
           <TextMenu editor={editor} />
           <ImageBlockMenu editor={editor} appendTo={menuContainerRef} /> 
