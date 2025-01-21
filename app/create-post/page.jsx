307305c5ec48ca0fa9671ac121cd7caff800bc93
madmad <ereny3gers10@gmail.com>
2025-01-21 16:44:34 +0330

    jsonld

--- app/create-post/page.jsx
+++ app/create-post/page.jsx
@@ -1,15 +1,36 @@
+import React from 'react';
 import CreatePost from "./create-post";
+import Head from 'next/head';
 
 export const metadata = {
-  metadataBase: new URL("https://www.atlasdoors.ir/create-post"),
+  metadataBase: new URL(`${process.env.NEXT_PUBLIC_BASE_URL}/create-post`),
   title: "create post",
 };
+
 const Page = () => {
+
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    "name": "create post",
+    "url": `${process.env.NEXT_PUBLIC_BASE_URL}/create-post`,
+    "description": "Create a new post on Atlas Door"
+  };
+
   return (
     <div>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta property="og:url" content={metadata.metadataBase} />
+        <meta name="description" content="Create a new post on Atlas Door" />
+        <meta property="og:title" content={metadata.title} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
+      </Head>
       <CreatePost />
     </div>
   );
 };
 
 export default Page;
+
--- app/edit-post/[title]/page.jsx
+++ app/edit-post/[title]/page.jsx
@@ -1,15 +1,36 @@
+import React from 'react';
 import EditPost from "./edit-post";
+import Head from 'next/head';
+
 export const metadata = {
-  metadataBase: new URL("https://www.atlasdoors.ir/edit-post"),
-  title :'edit post',
+  metadataBase: new URL(`${process.env.NEXT_PUBLIC_BASE_URL}/edit-post`),
+  title: 'edit post',
 }
+
 const Page = ({ params }) => {
+
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    "name": "edit post",
+    "url": `${process.env.NEXT_PUBLIC_BASE_URL}/edit-post`,
+    "description": "Edit an existing post on Atlas Door"
+  };
 
   return (
     <div>
-        <EditPost params={params}/>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta property="og:url" content={metadata.metadataBase} />
+        <meta name="description" content="Edit an existing post on Atlas Door" />
+        <meta property="og:title" content={metadata.title} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
+      </Head>
+      <EditPost params={params} />
     </div>
   );
 };
 
 export default Page;
+
